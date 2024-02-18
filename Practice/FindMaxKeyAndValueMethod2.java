934fc3aa944df2612c9718458a432617b4e92384
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-02-19 00:07:04 +0500

    FindMaxKeyAndValueMethod2.java created

---
+++ Practice/FindMaxKeyAndValueMethod2.java
@@ -0,0 +1,35 @@
+import java.util.Map; //interface
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set; //interface
+import java.util.HashSet;
+import java.util.Optional;
+
+class FindMaxKeyAndValueMethod2 {
+    
+    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 1, 5, 10, 19, -2);
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = 1;
+        for(Integer num : list) {
+            map.put(i, num);
+            i++;
+        }
+        System.out.println(map);
+        System.out.println("**********************************");
+        System.out.println(findMaxValueKey(map));
+    }
+    
+    public static Integer findMaxValueKey(Map<Integer, Integer> map)
+    {
+        if(map.isEmpty()) return null;
+        
+        Optional<Map.Entry<Integer, Integer>> maxEntry = map.entrySet().stream().max(Map.Entry.comparingByValue());
+        
+        Integer maxKey = maxEntry.map(Map.Entry::getKey).orElse(null);
+        
+        return maxKey;
+    }
+}
