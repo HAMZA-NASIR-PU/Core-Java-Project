df8449e8eb65016ce4ed48170a3891b904561ca9
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-03-04 17:43:42 +0500

    Bootstrap added in angular 15 project

--- Angular15 Projects/my-app/src/app/app-routing.module.ts
+++ Angular15 Projects/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodosComponent } from './pages/view-todos/view-todos.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home - Todo Manager'
+  },
+  {
+    path: 'add-todo',
+    component: AddTodoComponent,
+    title: 'Home - Todo Manager'
+  },
+  {
+    path: 'view-todos',
+    component: ViewTodosComponent,
+    title: 'Home - Todo Manager'
+  },
+  {
+    path: 'update-todo',
+    component: UpdateTodoComponent,
+    title: 'Home - Todo Manager'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular15 Projects/my-app/src/app/app.module.ts
+++ Angular15 Projects/my-app/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { ViewTodosComponent } from './pages/view-todos/view-todos.component';
+import { UpdateTodoComponent } from './pages/update-todo/update-todo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    HomeComponent,
+    AddTodoComponent,
+    ViewTodosComponent,
+    UpdateTodoComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
